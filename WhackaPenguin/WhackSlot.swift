ebf2e5400ac0a4c194e5a20e5875b61fc0649698
Ali•Gorithm <alisher.coder@gmail.com>
2017-06-29 21:23:56 -0400

    firstCommit1stLevelComplete

---
+++ WhackaPenguin/WhackSlot.swift
@@ -0,0 +1,35 @@
+//
+//  WhackSlot.swift
+//  Whack-a-Penguin
+//
+//  Created by Alisher Abdukarimov on 6/29/17.
+//  Copyright © 2017 MrAliGorithm. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class WhackSlot: SKNode {
+    
+    var charNode: SKSpriteNode!
+    
+    func configure(at position: CGPoint) {
+        self.position = position
+        
+        let sprite = SKSpriteNode(imageNamed: "whackHole")
+        addChild(sprite)
+        
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        cropNode.maskNode = nil
+        
+        charNode = SKSpriteNode(fileNamed: "penguinGood")
+        charNode.position = CGPoint(x: 0, y: -90)
+        charNode.name = "character"
+        cropNode.addChild(charNode)
+        
+        addChild(cropNode)
+    }
+    
+}
